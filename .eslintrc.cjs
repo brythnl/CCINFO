b80e0cb0b0f646e1f113e98d89f48f981f613b93
Bryan <bryanathanaelj@gmail.com>
2023-10-22 02:54:32 +0200

    build: configure eslint and prettier for typescript and nuxt

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {},
+};
