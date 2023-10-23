46ac6b0b10fc2598fe4f2df439995c780bc667b4
Bryan <bryanathanaelj@gmail.com>
2023-10-23 09:19:46 +0200

    build: add vuetify plugin

---
+++ plugins/vuetify.ts
@@ -0,0 +1,10 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+export default defineNuxtPlugin ((app) => {
+  const vuetify = createVuetify({
+    // configuration here
+  })
+  app.vueApp.use(vuetify)
+})
