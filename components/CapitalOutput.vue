2826fdcd048ce86b35553643a3630ff78de64e42
fp5774s <fachrialdimaspp15@gmail.com>
2023-10-27 02:03:05 +0200

    feature: add placeholder for graph
    add placeholder for graph (component);
    add image for placeholder;
    implement "reiten grafik";
    simple styling throung vuetify;

--- components/CapitalOutput.vue
+++ components/CapitalOutput.vue
@@ -6,12 +6,20 @@ const props = defineProps({
 
 <template>
   <div>
-    <div v-for="(items,index) in data">
-    <h2>{{index}}</h2>
-    <ul>
-      <li v-for="(item,i) in items"> {{ i }} = {{ item }}</li>
-    </ul>
-    </div>
+    <h1 v-if="!$data">API Visualization</h1>
+    <v-expansion-panels v-else>
+      <v-expansion-panel>
+      <v-expansion-panel-title>API Visualization</v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <div v-for="(items,index) in data">
+          <h2>{{index}}</h2>
+          <ul>
+            <li v-for="(item,i) in items"> {{ i }} = {{ item }}</li>
+          </ul>
+        </div></v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    
   </div>
 </template>
 
