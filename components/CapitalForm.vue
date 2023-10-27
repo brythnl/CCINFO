3be7d3ea3d5fb362ed8934f612fbf5e7e687b633
fp5774s <fachrialdimaspp15@gmail.com>
2023-10-27 02:03:05 +0200

    feature: add placeholder for graph
    add placeholder for graph (component);
    add image for placeholder;
    implement "reiten grafik";
    simple styling throung vuetify;

--- components/CapitalForm.vue
+++ components/CapitalForm.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 
-const API_TOKEN = ""
+const API_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4ZksxTW95NkVidjktcjNEQ3IwMXJtemVsY3FkMUpUbVJDaGh6YkxRblpnIn0.eyJleHAiOjE2OTgzNjgxMDEsImlhdCI6MTY5ODM2NDUwMSwiYXV0aF90aW1lIjoxNjk4MzM3MDA4LCJqdGkiOiIwYmEzOWY3Mi1lNGZiLTQ0Y2UtYTMxMi0wN2E1YTExZGRhY2UiLCJpc3MiOiJodHRwczovL2xvZ2luLnBvcnRhbC5haXhpZ28uY2xvdWQvcmVhbG1zL2FwcHMiLCJhdWQiOlsid2Vic2l0ZSIsImRlbW8iXSwic3ViIjoiZmFjaHJpYWwucGVyZGFuYUBhbHVtbmkuZmgtYWFjaGVuLmRlIiwidHlwIjoiSUQiLCJhenAiOiJ3ZWJzaXRlIiwic2Vzc2lvbl9zdGF0ZSI6IjM0YmUyODk4LTljY2EtNDczNy04YWY3LWMwZTQ4NDYzNjAzNCIsImF0X2hhc2giOiJFZHY3ckZRejRrWEZRTTZsY3RKY1JBIiwiYWNyIjoiMCIsInNpZCI6IjM0YmUyODk4LTljY2EtNDczNy04YWY3LWMwZTQ4NDYzNjAzNCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJyb2xlcyI6WyJHVUVTVCJdLCJuYW1lIjoiRmFjaHJpYWwgRGltYXMgUHV0cmEgUGVyZGFuYSIsInByZWZlcnJlZF91c2VybmFtZSI6ImZhY2hyaWFsLnBlcmRhbmFAYWx1bW5pLmZoLWFhY2hlbi5kZSIsImdpdmVuX25hbWUiOiJGYWNocmlhbCBEaW1hcyBQdXRyYSIsImZhbWlseV9uYW1lIjoiUGVyZGFuYSIsInRlbmFudCI6Imd1ZXN0IiwiZW1haWwiOiJmYWNocmlhbC5wZXJkYW5hQGFsdW1uaS5maC1hYWNoZW4uZGUifQ.ZlG58aQdiNfSneBTHOvIzrwKzlgT4O0_M33nxhJb3qa1ITbXctI65A-32rXVVzqaMHYBbMOY4z0duSBnqfXyhrttZ8vENiX8Z6w7oWVo9VfFe_R7qkNIqznOSm9ZTl5fPbMGd1PS87r2XXkrggtfk9D4006JRil94H7pCeaF7xT3wcu_j4GdnOB7cDKZjpgBUZRIf2LCYuMv3SXzuZa3ipOE9hr01Sh6IItl3bGjzPqR1L9SKpX92DylmOjxRyGQqrXd6WBH2y5i991cmrjIodyFbTNpPYNqLypfkyIXgbfYIOj-Xt-zEtYQkTtAGT1jH94TboY7gCkZwxdsi1fn_w"
 
 const capitalInput = reactive({
   beginDate: "",
@@ -27,10 +27,11 @@ async function getEndCapital() {
 </script>
 
 <template>
+  <h1>Capital Calculation</h1>
   <v-form>
     <v-container>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col>
           <input
             type="date"
             v-model="capitalInput.beginDate"
@@ -38,7 +39,7 @@ async function getEndCapital() {
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col>
           <input
             type="date"
             v-model="capitalInput.endDate"
@@ -46,7 +47,7 @@ async function getEndCapital() {
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col>
           <v-text-field
             v-model="capitalInput.interestRate"
             label="Interest rate"
@@ -58,7 +59,7 @@ async function getEndCapital() {
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col >
           <v-select
             v-model="capitalInput.interestCalculation"
             clearable
@@ -68,7 +69,7 @@ async function getEndCapital() {
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col >
           <v-text-field
             v-model="capitalInput.savingRate"
             label="Saving rate"
@@ -80,7 +81,7 @@ async function getEndCapital() {
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" md="4">
+        <v-col >
           <v-btn
             :disabled="loading"
             :loading="loading"
--- pages/capital.vue
+++ pages/capital.vue
@@ -9,14 +9,25 @@ function calculateForm(res:[]){
 </script>
 
 <template>
-  <div>
-    <div>
+  <v-container class="h-100 ma-10">
+    <v-row class="h-100">
+      <v-col cols="3" class="h-100 rounded-lg bg-cyan-darken-3 ma-2">
+    <div class="w-100 h-100">
       <capital-form @calculated="calculateForm"/>
     </div>
-    <div>
+  </v-col>
+  <v-col class="h-100 rounded-lg bg-cyan-darken-3 ma-2">
+    <div class="w-100 h-100">
+      <capital-graph/>
+    </div>
+  </v-col>
+    <v-col class="h-100 rounded-lg bg-cyan-darken-3 ma-2">
+    <div class="w-100 h-100">
       <capital-output :data="newResult"/>
     </div>
-  </div>
+  </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <style scoped></style>
