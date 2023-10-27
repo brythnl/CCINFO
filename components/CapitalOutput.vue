9997f8c19ef67174135b6389f96d70eece154e98
Bryan <bryanathanaelj@gmail.com>
2023-10-27 16:07:15 +0200

    refactor: change for loop variables

--- components/CapitalOutput.vue
+++ components/CapitalOutput.vue
@@ -8,20 +8,22 @@ defineProps<{
 
 <template>
   <div>
-    <h1 v-if="!$data">API Visualization</h1>
+    <h1 v-if="!capitalData">API Visualization</h1>
     <v-expansion-panels v-else>
       <v-expansion-panel>
-      <v-expansion-panel-title>API Visualization</v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <div v-for="(items,index) in data">
-          <h2>{{index}}</h2>
-          <ul>
-            <li v-for="(item,i) in items"> {{ i }} = {{ item }}</li>
-          </ul>
-        </div></v-expansion-panel-text>
+        <v-expansion-panel-title>API Visualization</v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <div v-for="(values, property) in capitalData">
+            <h2>{{ property }}</h2>
+            <ul>
+              <li v-for="(value, valueType) in values">
+                {{ valueType }} = {{ value }}
+              </li>
+            </ul>
+          </div></v-expansion-panel-text
+        >
       </v-expansion-panel>
     </v-expansion-panels>
-    
   </div>
 </template>
 
