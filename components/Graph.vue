af1b916c475657e83b73cd1f8dd1f0efbebf800a
Redouan <redouan.akdim@web.de>
2023-11-02 22:07:51 +0100

    feat:Created a highchart visualization component

---
+++ components/Graph.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Chart as highcharts } from "highcharts-vue";
+</script>
+
+<template>
+  <h1>Graph</h1>
+  <highcharts
+    :options="{
+      chart: {
+        type: 'area', // Basic area chart
+      },
+      xAxis: {
+        title: {
+          text: 'Years',
+        },
+        //categories: ['Apples', 'Bananas', 'Oranges'],
+      },
+      yAxis: {
+        title: {
+          text: 'Capital accumulation',
+        },
+      },
+      series: [
+        {
+          data: [1, 2, 3], // sample data
+        },
+      ],
+    }"
+  ></highcharts>
+</template>
+
+<style scoped></style>
