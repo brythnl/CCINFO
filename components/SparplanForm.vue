a3eb7a2bd60167ac3a00b957b2706dc649ed6ee3
fp5774s <fachrialdimaspp15@gmail.com>
2023-11-02 19:29:23 +0100

    feature: multiple one time payment
    add feature to add multiple one time payments in addition to start capital

--- components/SparplanForm.vue
+++ components/SparplanForm.vue
@@ -1,27 +1,45 @@
 <script lang="ts" setup>
-const toFind = ref("")
-const dateTime = new Date().toISOString().split('T')[0].toString().replace('/T/','');
+
+const date = new Date();
+const todayDate = date.toISOString().split('T')[0].toString().replace('/T/','');
+date.setFullYear(date.getFullYear()+10);
+const inTenYears = date.toISOString().split('T')[0].toString().replace('/T/','');
+
+
 const savingInput=reactive({
-  startCapital:0,
-  savingRate:0,
+  beginDate:todayDate,
+  endDate:inTenYears,
   interestRate:0,
-  startDate:dateTime,
-  endDate:"",
   interestCalculation: "YEARLY",
+  reductionFactor:0,
+  dynamicSavingRateFactor:0,
+  savingPlanBegin:"",
+  savingPlanEnd:"",
+  oneTimeInvestment:[0],
+  oneTimeInvestmentDate:[todayDate],
+  savingRate:0,
   endCapital:0,
 })
+
+const toFind = ref("")
+const einmalZahlung = ref(0);
+
+
+
+
 function reset(){
-  savingInput.startCapital=0;
+  savingInput.oneTimeInvestment=[0];
+  oneTimeInvestmentDate:[todayDate];
   savingInput.savingRate=0;
   savingInput.interestRate=0;
-  savingInput.endDate="";
+  savingInput.endDate=inTenYears;
   savingInput.endCapital=0;
+  einmalZahlung.value=0;
 }
 function getData(){
   savingInput.interestRate = savingInput.interestRate/100;
   console.log(savingInput);
 }
-
 function determineEndpoint(item: string){
   let endpoint: string ='';
   switch(item){
@@ -49,7 +67,6 @@ function determineEndpoint(item: string){
   }
   console.log(`Es wird ${endpoint} aufgerufen`)
 }
-
 </script>
 
 <template>
@@ -61,47 +78,85 @@ function determineEndpoint(item: string){
       v-model="toFind"
       @update:model-value="reset">
         <v-row >
-          <v-radio label="Startkapital (€)" value="startkapital" @click="determineEndpoint('startcapital')"></v-radio>
+          <v-radio label="Startkapital" value="startkapital" @click="determineEndpoint('startcapital')"></v-radio>
         </v-row>
         <v-row>
         <v-col class="pa-0">
           <v-text-field
+            prefix="€"
             style="border: 3px solid #00476B;"
             density="compact"
-            v-model="savingInput.startCapital"
+            v-model="savingInput.oneTimeInvestment[0]"
             required
             hide-details
-            placeholder="Startkapital (€)"
+            placeholder="Startkapital"
             type="number"
             step="0.01"
             class="bg-white rounded"
             :disabled="toFind==''||toFind=='startkapital'"
           ></v-text-field>
         </v-col>
-        <v-col cols="3" class="ma-auto">
+        <v-col cols="2" class="pa-0">
           <v-btn style="background-color: inherit;" flat>
-            <v-avatar class="ma-auto">
+            <v-avatar>
                 <v-img src="~/assets/Information-Icon.png"></v-img>
             </v-avatar>
-              Details
             <v-tooltip activator="parent" location="end" class="w-50">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-               sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              This parameter defines any number of one-time cash inflows.
             </v-tooltip>
           </v-btn>
         </v-col>
+        <v-col cols="4">
+          <v-row><h5>weitere Einmalzahlung</h5></v-row>
+          <v-row>
+          <v-btn-group 
+          density="compact"
+          variant="tonal"
+          color="#00476B" >
+            <v-btn style="border: solid 1px black;" @click="()=>einmalZahlung++" :disabled="toFind==''||toFind=='startkapital'">+</v-btn>
+            <v-btn style="border: solid 1px black;" @click="()=>{einmalZahlung>0?einmalZahlung--:einmalZahlung=0;}" :disabled="toFind==''||toFind=='startkapital'">-</v-btn>
+          </v-btn-group>
+        </v-row>
+        </v-col>
+      </v-row>
+      <v-row v-for="n in einmalZahlung">
+        <v-col class="pa-0">
+          <v-text-field
+            prefix="€"
+            style="border: 3px solid #00476B;"
+            density="compact"
+            v-model="savingInput.oneTimeInvestment[n]"
+            hide-details
+            placeholder="weitere Einmalzahlung"
+            type="number"
+            class="bg-white rounded ma-3"
+            :disabled="toFind==''||toFind=='startkapital'"
+          ></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field
+            style="border: 3px solid #00476B;"
+            density="compact"
+            v-model="savingInput.oneTimeInvestmentDate[n]"
+            hide-details
+            type="date"
+            class="bg-white rounded"
+            :disabled="toFind==''||toFind=='startkapital'"
+          ></v-text-field>
+        </v-col>
       </v-row>
       <v-row>
-        <v-radio label="Sparrate (€)" value="sparrate" @click="determineEndpoint('savingrate')"></v-radio></v-row>
+        <v-radio label="Sparrate" value="sparrate" @click="determineEndpoint('savingrate')"></v-radio></v-row>
       <v-row>
         <v-col class="pa-0">
           <v-text-field
+            prefix="€"
             style="border: 3px solid #00476B;"
             density="compact"
             v-model="savingInput.savingRate"
             required
             hide-details
-            placeholder="Sparrate (€)"
+            placeholder="Sparrate"
             type="number"
             step="0.01"
             class="bg-white rounded"
@@ -113,7 +168,6 @@ function determineEndpoint(item: string){
             <v-avatar class="ma-auto">
                 <v-img src="~/assets/Information-Icon.png"></v-img>
             </v-avatar>
-              Details
             <v-tooltip activator="parent" location="end" class="w-50">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
@@ -122,16 +176,17 @@ function determineEndpoint(item: string){
         </v-col>
       </v-row>
       <v-row>
-        <v-radio label="Sparzins (%)" value="sparzins" @click="determineEndpoint('interestrate')"></v-radio></v-row>
+        <v-radio label="Sparzins" value="sparzins" @click="determineEndpoint('interestrate')"></v-radio></v-row>
       <v-row>
         <v-col class="pa-0">
           <v-text-field
+            suffix="%"
             style="border: 3px solid #00476B;"
             density="compact"
             v-model="savingInput.interestRate"
             required
             hide-details
-            placeholder="Sparzins (%)"
+            placeholder="Sparzins"
             type="number"
             step="1"
             class="bg-white rounded"
@@ -143,7 +198,6 @@ function determineEndpoint(item: string){
             <v-avatar class="ma-auto">
                 <v-img src="~/assets/Information-Icon.png"></v-img>
             </v-avatar>
-              Details
             <v-tooltip activator="parent" location="end" class="w-50">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
@@ -170,7 +224,6 @@ function determineEndpoint(item: string){
             <v-avatar class="ma-auto">
                 <v-img src="~/assets/Information-Icon.png"></v-img>
             </v-avatar>
-              Details
             <v-tooltip activator="parent" location="end" class="w-50">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
@@ -178,15 +231,16 @@ function determineEndpoint(item: string){
           </v-btn>
         </v-col>
       </v-row>
-      <v-row><v-radio label="Endkapital (€)" value="endkapital" @click="determineEndpoint('endcapital')"></v-radio></v-row>
+      <v-row><v-radio label="Endkapital" value="endkapital" @click="determineEndpoint('endcapital')"></v-radio></v-row>
       <v-row>
         <v-col class="pa-0">
           <v-text-field
+            prefix="€"
             style="border: 3px solid #00476B;"
             density="compact"
             v-model="savingInput.endCapital"
             hide-details
-            placeholder="Endkapital (€)"
+            placeholder="Endkapital"
             type="number"
             step="0.01"
             class="bg-white rounded"
@@ -198,7 +252,6 @@ function determineEndpoint(item: string){
             <v-avatar class="ma-auto">
                 <v-img src="~/assets/Information-Icon.png"></v-img>
             </v-avatar>
-              Details
             <v-tooltip activator="parent" location="end" class="w-50">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
