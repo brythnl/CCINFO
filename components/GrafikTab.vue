b733655e33df930275bae8c24e3fd7ede1a424b2
fp5774s <fachrialdimaspp15@gmail.com>
2023-11-04 22:09:33 +0100

    feature: tabs for graph
    add feature tabs for graph to change between actual graph, previous graph, comparison and table

---
+++ components/GrafikTab.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const planSelect = ref("saving")
+const emit = defineEmits(['grafikUpdate'])
+function grafikUpdate(){
+  console.log(planSelect.value);
+  emit('grafikUpdate',planSelect.value);
+}
+</script>
+
+<template>
+  <div>
+    <v-tabs
+    v-model="planSelect"
+    @update:model-value="grafikUpdate"
+    color="#B2B2B2" bg-color="#00476B"
+    align-tabs="end">
+      <v-tab value="aktuell" elevation="5">
+        <h3>Grafik aktuell</h3>
+      </v-tab>
+      
+      <v-tab value="vorher" elevation="5">
+        <h3>Grafik vorher</h3>
+      </v-tab>
+
+      <v-tab value="vergleich" elevation="5">
+        <h3>Vergleich</h3>
+      </v-tab>
+
+      <v-tab value="tabelle" elevation="5">
+        <h3>Tabelle</h3>
+      </v-tab>
+
+    </v-tabs>
+  </div>
+</template>
+
+<style scoped></style>
