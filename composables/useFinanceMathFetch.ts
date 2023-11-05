c79c644acb963af8efedb984512afd8e78cac7d6
Bryan <bryanathanaelj@gmail.com>
2023-11-05 14:05:23 +0100

    feature: create wrapper function to fetch from Finance Math API

---
+++ composables/useFinanceMathFetch.ts
@@ -0,0 +1,26 @@
+import type { financeMathInput } from "~/types/index.d.ts"
+
+export const useFinanceMathFetch = async <DataT>(endpoint: string, paramInput: financeMathInput) => {
+  let API_TOKEN = ""
+
+  let url = `https://demo.portal.aixigo.cloud:443/finance-math/${endpoint}?`
+
+  const keys = Object.keys(paramInput) as Array<keyof typeof paramInput>
+  for (const [i, key] of keys.entries()) {
+    if (paramInput[key] !== undefined) {
+      url += `${String(key)}=${paramInput[key]}`
+      if (i <= keys.length - 1) {
+        url += "&"
+      }
+    }
+  }
+
+  return useFetch<DataT>(url,
+    {
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    }
+  )
+}
