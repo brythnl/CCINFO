fd3d42ee93c83f507f87e20d8fb8cbb0195cf94c
fp5774s <fachrialdimaspp15@gmail.com>
2023-11-06 08:13:03 +0100

    feature: add output for request
    add output for API req
    style for seach feature

--- components/ApiVisualization.vue
+++ components/ApiVisualization.vue
@@ -1,6 +1,14 @@
 <script lang="ts" setup>
 
 //Dummy Data
+const req = ref({
+  "end": "2030-01-01",
+  "savingRate":100,
+  "oneTimeInvestment":[1000],
+  "oneTimeInvestmentDate":["2020-01-01"],
+  "interestRate":0.05
+})
+
 const data = ref({
   "capitalResult": {
     "capitalAmount": 42332.44262861051,
@@ -42,22 +50,29 @@ const find = ref("a");
   </div>
   <div>
     <v-btn-toggle v-model="find">
-      <v-btn value="startCapital">Startkapital</v-btn>
-      <v-btn value="savingRate">Sparrate</v-btn>
-      <v-btn value="interestRate">Sparzins</v-btn>
-      <v-btn value="end">Enddatum</v-btn>
-      <v-btn value="capitalAmount">Endkapital</v-btn>
+      <v-btn value="startInvestment"><v-chip>Startkapital</v-chip></v-btn>
+      <v-btn value="savingRate"><v-chip>Sparrate</v-chip></v-btn>
+      <v-btn value="interestRate"><v-chip>Sparzins</v-chip></v-btn>
+      <v-btn value="end"><v-chip>Enddatum</v-chip></v-btn>
+      <v-btn value="capitalAmount"><v-chip>Endkapital</v-chip></v-btn>
     </v-btn-toggle>
   </div>
   <v-card class="overflow-y-auto bg-grey-darken-4" max-height="800" variant="outlined">
     <v-card-item>
       <v-card-text>
+        <code>Request {</code>
+        <div v-for="(item,index) in req">
+          <code class="ps-5" :class="index==find?'bg-green-accent-2':''">{{ index }}": {{ item }} <br></code>
+        </div>
+        <code>}</code>
+        <code>Response {</code>
         <div v-for="(item,index) in data">
-          <code :class="index==find?'bg-green-accent-2':''">{{ index }}": { <br></code>
+          <code class="ps-5" :class="index==find?'bg-green-accent-2':''">{{ index }}": { <br></code>
           <code v-for="(subitem,subindex) in item" class="ps-10" :class="subindex==find?'bg-green-accent-2':''">
             "{{ subindex }}": {{ subitem }},<br>
           </code>
-          <code>} <br></code>
+          <code class="ps-5">} <br></code>
+          <code>}</code>
         </div>
       </v-card-text>
     </v-card-item>
