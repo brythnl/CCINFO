0d10dbeef5c5fe36197ed310aedfd710a4976788
Bryan <bryanathanaelj@gmail.com>
2023-11-11 00:21:08 +0100

    refactor: - Remove async - Add condition for empty end date strings - Change method to remove trailing & symbol

--- composables/useFinanceMathFetch.ts
+++ composables/useFinanceMathFetch.ts
@@ -1,18 +1,17 @@
 import type { financeMathInput } from "~/types/index.d.ts"
 
-export const useFinanceMathFetch = async <DataT>(endpoint: string, paramInput: financeMathInput, API_TOKEN: string) => {
+export const useFinanceMathFetch = <DataT>(endpoint: string, paramInput: financeMathInput, API_TOKEN: string) => {
   let url = `https://demo.portal.aixigo.cloud:443/finance-math/${endpoint}?`
 
   // append input from form as query parameters for the API call
   const keys = Object.keys(paramInput) as Array<keyof typeof paramInput>
   for (const [i, key] of keys.entries()) {
-    if (paramInput[key] !== undefined || paramInput[key] !== 'endpoint') {
-      url += `${String(key)}=${paramInput[key]}`
-      if (i <= keys.length - 2) {
-        url += "&"
-      }
+    if (paramInput[key] !== (undefined || "") && key !== 'endpoint') {
+      url += `${String(key)}=${paramInput[key]}&`
     }
   }
+  url = url.slice(0, -1) // Remove trailing '&'
+  console.log(url)
 
   return useFetch<DataT>(url,
     {
