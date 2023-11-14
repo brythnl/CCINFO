5535252aabc666d0b062192dfa4ff6750acb3d6e
Bryan <bryanathanaelj@gmail.com>
2023-11-14 10:55:23 +0700

    chore: document form-related constants and functions

--- utils/formUtils.ts
+++ utils/formUtils.ts
@@ -1,9 +1,11 @@
-import type { financeMathInput} from "~/types/index.d.ts"
+import type { financeMathInput } from "~/types/index.d.ts"
+
+// Date constants for form default values (todayDate, inTenYears, inTwentyYears)
+export const todayDate = formatDate(new Date())
+
 function formatDate(date: Date) {
   return date.toISOString().split('T')[0].toString().replace('/T/','');
 }
-
-export const todayDate = formatDate(new Date())
 
 const date = new Date();
 date.setFullYear(date.getFullYear() + 10);
@@ -11,6 +13,7 @@ export const inTenYears = formatDate(date)
 date.setFullYear(date.getFullYear() + 10);
 export const inTwentyYears = formatDate(date)
 
+// format the decimal point of a number into a comma
 export const formatNumber = (num: number): number => {
   return Number(num
     .toString()
@@ -18,17 +21,20 @@ export const formatNumber = (num: number): number => {
   );
 }
 
+// find the earliest date in an array
 export const findSmallestDate = (dates: string[]): string => {
   dates.sort((a: string, b: string) => new Date(a).valueOf() - new Date(b).valueOf())
   return dates[0];
 }
 
+// find the latest date in an array
 export const findBiggestDate = (dates: string[]): string => {
   dates.sort((a: string, b: string) => new Date(b).valueOf()- new Date(a).valueOf())
   return dates[0];
 }
 
-export const validateInput = (userInput:financeMathInput): void => {
+// modify user input to follow API parameter requirements
+export const validateInput = (userInput: financeMathInput): void => {
      // Set decimal separator to dot and make numbers to integer
      userInput.endValue = Math.round(formatNumber(userInput.endValue));
      userInput.savingRate = Math.round(formatNumber(userInput.savingRate));
@@ -41,7 +47,8 @@ export const validateInput = (userInput:financeMathInput): void => {
       userInput.begin = findSmallestDate(tmp)
  }
 
-export const setEndDateToBiggestDate = (userInput:financeMathInput): void => {
+// set end date value as the latest date found from all date inputs (oneTimeInvestmentDate, savingPlanEnd, end)
+export const setEndDateToBiggestDate = (userInput: financeMathInput): void => {
   if (userInput.endpoint == 'end-date') return;
   let tmp: string[] = (JSON.parse(JSON.stringify(userInput.oneTimeInvestmentDate)))
   tmp.push(JSON.parse(JSON.stringify(userInput.savingPlanEnd)))
