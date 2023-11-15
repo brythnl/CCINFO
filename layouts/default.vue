5f55598a14237caa6a0f0282e82282683fbdb1c9
claudiastrehlemayo <claudia@strehle.net>
2023-11-15 19:18:35 +0100

    header and logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,12 @@
 <template>
-  <slot />
+  <div>
+    <header class="shadow-sm bg-white">
+      <img class="pa-5" src="https://assets-global.website-files.com/60d5bca841056859df8738f0/60dec34edc0691ce3efed5f1_Logo.svg" alt="logo">
+    </header>
+  </div>
+  <div>
+    <slot/>
+  </div>
 </template>
+<script setup lang="ts">
+</script>
