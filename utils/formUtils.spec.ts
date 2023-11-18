92bb36729d536888d3faca795aa17845f3696cc7
Bryan <bryanathanaelj@gmail.com>
2023-11-18 10:21:36 +0700

    test: test if find[Smallest/Biggest]Date returns its corresponding correct dates

--- utils/formUtils.spec.ts
+++ utils/formUtils.spec.ts
@@ -3,7 +3,26 @@ import * as formUtils from './formUtils'
 
 describe('formUtils', () => {
   test('date is correctly formatted into a string', () => {
-    console.log(new Date(2023, 1, 1))
     expect(formUtils.formatDate(new Date(2023, 1, 1))).toBe("2023-01-31")
+  })
+
+  test('earliest date is returned from a array of dates', () => {
+    expect(formUtils.findSmallestDate([
+      "2023-05-27",
+      "2023-02-01",
+      "2023-04-18",
+      "2023-09-10",
+      "2023-01-02"
+    ])).toBe("2023-01-02")
+  })
+
+  test('latest date is returned from a array of dates', () => {
+    expect(formUtils.findBiggestDate([
+      "2023-05-27",
+      "2023-02-01",
+      "2023-04-18",
+      "2023-09-10",
+      "2023-01-02"
+    ])).toBe("2023-09-10")
   })
 })
