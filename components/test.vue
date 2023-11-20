47d456dafbdd0a5a136b1705a2bdf742341abdb9
claudiastrehlemayo <claudia@strehle.net>
2023-11-20 16:22:43 +0100

    Einheit hinter Datums-Differenz

--- components/test.vue
+++ components/test.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="pa-5">
-    <h4 class="font-bold">Änderungen</h4>
-    <v-table >
+  <div class="pb-10">
+    <h4 class="font-bold pb-3"><v-icon>mdi-swap-horizontal</v-icon> Ihre Änderungen</h4>
+    <v-table class="border-2 rounded-lg">
       <thead>
       <tr>
         <th>Feld</th>
@@ -15,14 +15,14 @@
         <td>{{ item.name }}</td>
         <td>{{ item.oldValue }}</td>
         <td>{{ item.newValue }}</td>
-        <td :class="{ 'red-text': item.difference < 0 }">{{ item.difference }}</td>
+        <td :class="{ 'red-text': item.difference.difference < 0 }">{{ item.difference.difference > 0 ? '+' : '' }}{{ item.difference.difference }} {{ item.difference.unit}}</td>
       </tr>
       </tbody>
     </v-table>
   </div>
-  <div class="pa-5">
-    <h4 class="font-bold">Ergebnis</h4>
-    <v-table >
+  <div>
+    <h4 class="font-bold pb-3"><v-icon>mdi-equal-box</v-icon> Ergebnis</h4>
+    <v-table class="border-2 rounded-lg">
       <thead>
       <tr>
         <th>Feld</th>
@@ -32,11 +32,11 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="item in combinedArray_resp">
+      <tr v-for="item in filteredCombinedArrayResp">
         <td>{{ item.name }}</td>
         <td>{{ item.oldValue }}</td>
         <td>{{ item.newValue }}</td>
-        <td :class="{ 'red-text': item.difference < 0 }">{{ item.difference }}</td>
+        <td :class="{ 'red-text': item.difference.difference < 0, 'green-text': item.difference.difference > 0 }">{{ item.difference.difference > 0 ? '+' : '' }}{{ item.difference.difference }} {{ item.difference.unit}}</td>
       </tr>
       </tbody>
     </v-table>
@@ -46,6 +46,9 @@
 <style scoped>
 .red-text {
   color: red;
+}
+.green-text {
+  color: green;
 }
 </style>
 
@@ -57,36 +60,8 @@ import {inTenYears, todayDate} from "../utils/formUtils";
 import {watch} from "vue";
 dayjs.extend(customParseFormat)
 
-/*
-const props = defineProps<{
-  apiRequest: financeMathInput,
-  apiResponse: financeMathResult
-}>()
-*/
 
 const frage = "capitalAmount";
-/*
-watch(() => props.apiRequest.endpoint, () => {
-  switch (props.apiRequest.endpoint) {
-    case "saving-start-value":
-      frage.value = "startValue";
-      break;
-    case "saving-rate":
-      frage.value = "savingRate";
-      break;
-    case "interest-rate":
-      frage.value = "interestRate";
-      break;
-    case "end-date":
-      frage.value = "end";
-      break;
-    case "capital":
-      frage.value = "capitalAmount";
-      break;
-  }
-})
-*/
-
 
 function createCombinedArray (oldObj, newObj){
   const combinedArray = [];
@@ -94,14 +69,15 @@ function createCombinedArray (oldObj, newObj){
   for (const key in oldObj) {
     const oldValue = oldObj[key];
     const newValue = newObj[key];
-    const difference = calculateDifference(oldValue, newValue);
+    const { difference, unit } = calculateDifference(oldValue, newValue);
 
     if(difference !== 0 && difference !== ""){
       combinedArray.push({
         name: names[key],
         oldValue: formatValue(oldValue),
         newValue: formatValue(newValue),
-        difference: formatValue(difference)
+        difference: formatDifference(difference, unit),
+        unit: unit
       });
     }
   }
@@ -110,15 +86,19 @@ function createCombinedArray (oldObj, newObj){
 }
 
 const formatValue = (value) => {
-  if (value === 'date') {
-    return dayjs(value).format('YYYY-MM-DD');
+  if (typeof value === 'number'){
+    return value.toFixed(2);
+  }
+  else if (dayjs(value).isValid()) {
+    return dayjs(value).format('DD.MM.YYYY');
   }
   return value;
 };
 
 const calculateDifference = (oldValue, newValue) => {
   if (typeof oldValue === "number" && typeof newValue === "number") {
-    return newValue - oldValue;
+    const difference = newValue - oldValue;
+    return { difference, unit: '' };
   }
   // überprüft, ob Werte Daten sind
   else if (dayjs(oldValue).isValid()) {
@@ -126,15 +106,34 @@ const calculateDifference = (oldValue, newValue) => {
     let new_date = dayjs(newValue);
     let diff = new_date.diff(old_date, 'month');
     if (diff >= 12 || diff <= -12) {
-      return new_date.diff(old_date, 'year');
+      return { difference: new_date.diff(old_date, 'year'), unit: 'Jahre' };
     } else {
-      return diff;
+      return { difference: diff, unit: 'Monate' };
     }
   }
   else {
-    return "";
+    return { difference: "", unit: '' };
   }
 }
+
+const formatDifference = (difference, unit) => {
+  if(unit == '' && typeof difference === 'number'){
+    return { difference: difference.toFixed(2), unit:unit };
+  }
+  return { difference: difference, unit: unit };
+};
+
+// Funktion zum Filtern des combinedArray_resp nach dem Namen "capitalAmount"
+const filterCombinedArrayResp = (dataArray, targetName) => {
+  const translation = names[targetName];
+  return dataArray.filter(item => item && item.name === translation);
+};
+
+const formatNumberWithThousandSeparator = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+};
+
+// Dummy Daten
 
 const old_req = {
   begin: "2023-01-01",
@@ -226,10 +225,15 @@ const names = {
   "startInvestment": "Startkapital"
 }
 
+
 const combinedArray_req = createCombinedArray(old_req, new_req);
 const combinedArray_resp = createCombinedArray(old_resp.capitalResult, new_resp.capitalResult);
+// Gefiltertes Array erstellen
+const filteredCombinedArrayResp = filterCombinedArrayResp(combinedArray_resp, frage);
 
+// Test
 console.log("Combined Array for old_req and new_req:", combinedArray_req);
 console.log("Combined Array for old_resp and new_resp:", combinedArray_resp);
+console.log("Filtered Combined Array for old_resp and new_resp:", filteredCombinedArrayResp);
 
 </script>
