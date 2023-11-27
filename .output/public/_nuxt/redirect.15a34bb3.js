59d81b91ad9dddb2f969b656aeffbc8fa8f6b293
Bryan <bryanathanaelj@gmail.com>
2023-11-27 09:45:35 +0100

    Revert "build: add production code"
    This reverts commit e17d8206101b238ed2cff30573df97293920621d.

--- .output/public/_nuxt/redirect.15a34bb3.js
+++
@@ -1,1 +0,0 @@
-import{bH as r,m as n}from"./entry.9c620d35.js";import{a as s}from"./auth.4a8b2e9f.js";const p={__name:"redirect",async setup(_){let e,t;const a=s();return[e,t]=r(()=>a.signinRedirectCallback()),await e,t(),n("/"),(c,o)=>null}};export{p as default};
