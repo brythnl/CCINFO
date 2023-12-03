855cb6bceb54023bc392a3d5c7f02260018a5408
claudiastrehlemayo <claudia@strehle.net>
2023-12-03 18:21:09 +0100

    Jahre, Series mit Tausenderpunkten

--- components/SeriesTable.vue
+++ components/SeriesTable.vue
@@ -13,30 +13,35 @@ console.log(props.apiResponse.capitalSeries);
 console.log(new Intl.NumberFormat().format(1000))
 
 function numberFormat(number){
-  return new Intl.NumberFormat().format(number);
+  const fixed = (number / 100).toFixed(0);
+  return new Intl.NumberFormat().format(fixed);
 }
 
 
 </script>
 
 <template>
-  <v-table>
+  <h1 class="flex justify-center pt-5 text-lg font-bold">Vermögens-Entwicklung</h1>
+  <v-table class="ma-5 border-2 rounded-lg overflow-y-auto " max-height="650" height="600">
     <thead>
       <tr>
         <th>Jahr</th>
+        <!--
         <th>Guthaben zu<br>Jahresbeginn</th>
         <th>Einzahlungen</th>
-        <th>Zinsgutschriften</th>
+        <th>Zinsgutschriften</th>-->
         <th>Neues Guthaben</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="(item, index) in props.apiResponse.capitalSeries" :key="index">
-        <td>{{ parseInt(props.apiRequest.begin.substring(0, 4)) + index }}</td>
+        <td>{{ parseInt(props.apiRequest.begin.substring(0, 4)) + index +1 }}</td>
+        <!--
+        <td>{{ index == 0 ? numberFormat(props.apiResponse.capitalResult.startInvestment) : neues_Guthaben }}</td>
         <td>test</td>
         <td>test</td>
-        <td>test</td>
-        <td>{{ numberFormat((item / 100).toFixed(0)) }}</td>
+        -->
+        <td>{{ neues_Guthaben = numberFormat(item) }}</td>
       </tr>
     </tbody>
   </v-table>
