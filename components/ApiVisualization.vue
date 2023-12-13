44251f63cb1d55934a42c17c40ac32ea2c503282
johannes1912 <johannesschneidewind@gmail.com>
2023-12-13 17:43:45 +0100

    finalized api visualisation

--- components/ApiVisualization.vue
+++ components/ApiVisualization.vue
@@ -62,8 +62,8 @@ watch(
 <!--Variante 1 <v-card class="overflow-y-auto bg-white mb-5 ml-3 mr-3 h-100 rounded-lg mt-5" max-height="700" height="700" variant="elevated" elevation="3">-->
 <!--<v-card class="overflow-y-auto bg-grey-lighten-4 h-100 rounded-lg mt-5" max-height="700" height="700" variant="elevated" elevation="1">-->
 <!--<v-card class="overflow-y-auto bg-primary h-100 rounded-lg mt-5" max-height="700" height="700" variant="outlined">-->
-  <v-card class="overflow-y-auto bg-primary h-100 rounded-lg mt-5" max-height="700" height="700" variant="outlined">
-    <v-card-item>
+<v-card class="overflow-y-auto bg-grey-lighten-4 h-100 rounded-lg mt-5" max-height="700" height="700" variant="">    
+  <v-card-item>
       <v-card-text>
         <code>Request {</code>
         <div v-for="(item, index) in props.apiRequest">
